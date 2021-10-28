63e67e21a20bcb8e7e956656df8386ca3f6cfd97
Anthony Furlow <90413314+cashyy1k@users.noreply.github.com>
2021-10-28 14:11:00 -0400

    New variables v0.0

--- new variables/variables_practice.py
+++ new variables/variables_practice.py
@@ -3,4 +3,9 @@
 # Common Data Types and variables in python
 
 life_count = 3 # Integer value, all whole values, +/- and 0.
+amount_balance = 4232.57 # Floating Point value, +/-.
+user_name = "Cashyy1k" # String, a collection of letters/numbers.
 
+print(life_count)
+print(amount_balance)
+print(user_name)
